fc7682443be1ee86d59a087937b99160b140f616
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-01-30 14:52:44 -0300

    Add [ otavio ] console command

--- otaviOS/Model/Model.swift
+++ otaviOS/Model/Model.swift
@@ -11,7 +11,7 @@ import UIKit
 class Model {
     static let instance = Model()
     
-    static let isDebugging: Bool = false
+    static let isDebugging: Bool = true
     
     //DEBUG OPTIONS--------------------------
     var isSoundMuted: Bool = false || isDebugging
@@ -186,9 +186,85 @@ class Model {
             //
             
             //otavio
-            "otavio" : "comando em construção",
-            "otavio --help" : "comando em construção",
-            "otavio -h" : "comando em construção",
+            "otavio" : """
+            ------------------------HELP------------------------
+            
+            Métodos:
+                [ name ] : Retorna o nome.
+                [ age ] : Retorna a idade.
+                [ nickname ] : Retorna o apelido.
+                [ address ] : Retorna o endereço de residência.
+                [ birth ] : Retorna a data de nascimento.
+                [ interests ] : Retorna os interesses.
+                [ email ] : Retorna o email.
+                [ occupation ] : Retorna a ocupação.
+                [ phone ] : Retorna o número de telefone móvel
+            
+            Exemplo: [ otavio name ]
+            """,
+            
+            "otavio --help" : """
+            ------------------------HELP------------------------
+            
+            Métodos:
+                [ name ] : Retorna o nome.
+                [ age ] : Retorna a idade.
+                [ nickname ] : Retorna o apelido.
+                [ address ] : Retorna o endereço de residência.
+                [ birth ] : Retorna a data de nascimento.
+                [ interests ] : Retorna os interesses.
+                [ email ] : Retorna o email.
+                [ occupation ] : Retorna a ocupação.
+                [ phone ] : Retorna o número de telefone móvel
+            
+            Exemplo: [ otavio name ]
+            """,
+            
+            "otavio -h" : """
+            ------------------------HELP------------------------
+            
+            Métodos:
+                [ name ] : Retorna o nome.
+                [ age ] : Retorna a idade.
+                [ nickname ] : Retorna o apelido.
+                [ address ] : Retorna o endereço de residência.
+                [ birth ] : Retorna a data de nascimento.
+                [ interests ] : Retorna os interesses.
+                [ email ] : Retorna o email.
+                [ occupation ] : Retorna a ocupação.
+                [ phone ] : Retorna o número de celular.
+            
+            Exemplo: [ otavio name ]
+            """,
+            
+            "otavio name" : "Nome: Otávio Baziewicz Filho",
+            
+            "otavio age" : "Idade: 21 Anos",
+            
+            "otavio nickname" : "Apelido: Baza",
+            
+            "otavio address" : "Rua José Zaleski, 674 - ap 344",
+            
+            "otavio birth" : "Data de nascimento: 16/05/1998",
+            
+            "otavio interests" : """
+            Interesses:
+                Segurança de dados
+                Programação
+                Negócios
+                Design de interação
+                Skateboard
+                Vôlei
+                Culinária
+                DIY
+                Organização
+            """,
+            
+            "otavio email" : "Email: otavio.baziewicz.filho@gmail.com",
+            
+            "otavio occupation" : "Ocupação: Desenvolvedor",
+            
+            "otavio phone" : "Celular: (41) 9 9689-2354",
             //
             
             //metasploit
